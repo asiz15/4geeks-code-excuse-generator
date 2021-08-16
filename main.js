cb854de2db76e35789fb403b25309cad7b60a996
Alex Silva Z <a.silvazapata@gmail.com>
2021-08-16 15:53:41 +0000

    remove all semicolons

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-const action = ['ate', 'peed', 'crushed', 'broke'];
-const what = ['my homework', 'the keys', 'the car'];
-const when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+const who = ['The dog', 'My grandma', 'His turtle', 'My bird']
+const action = ['ate', 'peed', 'crushed', 'broke']
+const what = ['my homework', 'the keys', 'the car']
+const when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying']
 
 
 
 const getRandomNumber = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 const generateExcuse = () => {
